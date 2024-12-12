9659fd1ad515d44aee9ef00911e7ad889ae7923a
Maxime Bompoil <maxime.bompoil2@gmail.com>
2024-12-12 09:56:03 +0100

    add pokemon details to team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { remove } from "../features/teamSlice"
+import { Link } from "react-router-dom"
 
 export const Team = () => {
     const dispatch = useDispatch()
@@ -11,11 +12,13 @@ export const Team = () => {
         <ul>
             {pokemonTeam.map((item, index) => (
                 <li key={index}>
-                    <p>{item.name}</p>
-                    <img
-                        src={item.img}
-                        alt={item.name}/>
-                   <button onClick={() => {dispatch(remove({name: item.name, img: item.img}))}}>-</button>
+                    <Link to={`/pokemon/${item.name}`}>
+                       <p>{item.name}</p>
+                        <img
+                          src={item.img}
+                          alt={item.name}/>
+                    </Link>
+                    <button onClick={() => {dispatch(remove({name: item.name, img: item.img}))}}>-</button>
                 </li>
             ))}
         </ul>
